e6145d30409edae30d9277e6f46a63be13c700d6
Chip Self <chipself9@gmail.com>
2014-11-26 12:39:47 -0500

    edit to formatting

--- oorps.rb
+++ oorps.rb
@@ -17,46 +17,31 @@ class Game
     when 'P'
       if computer_choice == :paper
         puts "It's a tie. \n You picked paper and the computer picked paper."
-
       elsif computer_choice == :rock
         puts "You picked paper and the computer picked rock. \n Paper wraps rock. \n You won!"
-
-
       elsif computer_choice == :scissors
         puts "You picked paper and the computer picked scissors. \n Scissors cut paper. \n Computer won!"
-
       end
 
     when 'R'
       if computer_choice == :rock
         puts "It's a tie. \n You picked rock and the computer picked rock."
-
       elsif computer_choice == :paper
         puts "You picked rock and the computer picked paper. \n Paper wraps rock. \n Computer won!"
-
-
       elsif computer_choice == :scissors
         puts "You picked rock and the computer picked scissors. \n Rock smashes scissors \n You won!"
-
       end
 
     when 'S'
       if computer_choice == :scissors
         puts "It's a tie. \n You picked scissors and the computer picked scissors."
-
       elsif computer_choice == :paper
         puts "You picked scissors and the computer picked paper. \n Scissors cut paper. \n You won!"
-
-
       elsif computer_choice == :rock
         puts "You picked scissors and the computer picked rock. \n Rock smashes scissors. \n Computer won!"
-
       end
-
     end
-
   end
-
 end
 
 
@@ -102,5 +87,4 @@ while 1 < 2
   computer_choice = computer_turn.computer_play()
   game = Game.new()
   game.determine_winner(computer_choice, human_choice)
-
 end
