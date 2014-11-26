ab4e5392251300366cf382f5dbfab4c0121a71a6
Chip Self <chipself9@gmail.com>
2014-11-25 19:44:03 -0500

    issue with tabs

--- oorps.rb
+++ oorps.rb
@@ -11,83 +11,83 @@ WINNING_RULES = { paper: 'rock', rock: 'scissors', scissors: 'paper' }
 class Game
 
 
-	def determine_winner(computer_choice, user_choice)
-		case user_choice
+  def determine_winner(computer_choice, user_choice)
+    case user_choice
 
-		when 'P'
-			if computer_choice == :paper
-				puts "It's a tie. \n You picked paper and the computer picked paper."
+    when 'P'
+      if computer_choice == :paper
+        puts "It's a tie. \n You picked paper and the computer picked paper."
 
-			elsif computer_choice == :rock
-				puts "You picked paper and the computer picked rock. \n Paper wraps rock. \n You won!"
-
-
-			elsif computer_choice == :scissors
-				puts "You picked paper and the computer picked scissors. \n Scissors cut paper. \n Computer won!"
-
-			end
-
-		when 'R'
-			if computer_choice == :rock
-				puts "It's a tie. \n You picked rock and the computer picked rock."
-
-			elsif computer_choice == :paper
-				puts "You picked rock and the computer picked paper. \n Paper wraps rock. \n Computer won!"
+      elsif computer_choice == :rock
+        puts "You picked paper and the computer picked rock. \n Paper wraps rock. \n You won!"
 
 
-			elsif computer_choice == :scissors
-				puts "You picked rock and the computer picked scissors. \n Rock smashes scissors \n You won!"
+      elsif computer_choice == :scissors
+        puts "You picked paper and the computer picked scissors. \n Scissors cut paper. \n Computer won!"
 
-			end
+      end
 
-		when 'S'
-			if computer_choice == :scissors
-				puts "It's a tie. \n You picked scissors and the computer picked scissors."
+    when 'R'
+      if computer_choice == :rock
+        puts "It's a tie. \n You picked rock and the computer picked rock."
 
-			elsif computer_choice == :paper
-				puts "You picked scissors and the computer picked paper. \n Scissors cut paper. \n You won!"
+      elsif computer_choice == :paper
+        puts "You picked rock and the computer picked paper. \n Paper wraps rock. \n Computer won!"
 
 
-			elsif computer_choice == :rock
-				puts "You picked scissors and the computer picked rock. \n Rock smashes scissors. \n Computer won!"
+      elsif computer_choice == :scissors
+        puts "You picked rock and the computer picked scissors. \n Rock smashes scissors \n You won!"
 
-			end
+      end
 
-		end
+    when 'S'
+      if computer_choice == :scissors
+        puts "It's a tie. \n You picked scissors and the computer picked scissors."
 
-	end
+      elsif computer_choice == :paper
+        puts "You picked scissors and the computer picked paper. \n Scissors cut paper. \n You won!"
+
+
+      elsif computer_choice == :rock
+        puts "You picked scissors and the computer picked rock. \n Rock smashes scissors. \n Computer won!"
+
+      end
+
+    end
+
+  end
 
 end
 
 
 class GameEngine
 
-	def prompt
-		puts "Play Paper Rock Scissors!"
-		puts "Choose one: (P/R/S) or Q to quit"
-	end
+  def prompt
+    puts "Play Paper Rock Scissors!"
+    puts "Choose one: (P/R/S) or Q to quit"
+  end
 
-	def human_choice
-		human_choice = gets.chomp.upcase
-		if human_choice == "Q"
-		 	exit
-		end
-		human_choice
-	end
+  def human_choice
+    human_choice = gets.chomp.upcase
+    if human_choice == "Q"
+       exit
+    end
+    human_choice
+  end
 
-	def computer_choice
-		computer_choice = WINNING_RULES.keys[rand(WINNING_RULES.length)]
-	end
+  def computer_choice
+    computer_choice = WINNING_RULES.keys[rand(WINNING_RULES.length)]
+  end
 
 end
 
 
 while 1 < 2
-	play = GameEngine.new()
-	play.prompt()
-	human_choice = play.human_choice()
+  play = GameEngine.new()
+  play.prompt()
+  human_choice = play.human_choice()
   computer_choice =	play.computer_choice()
-	game = Game.new()
-	game.determine_winner(computer_choice, human_choice)
+  game = Game.new()
+  game.determine_winner(computer_choice, human_choice)
 
 end
